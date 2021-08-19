19f3129ae64c3aa783fa7a35c93c068b0829f8b1
JungHyun0115 <agnes.ojh@gmail.com>
2021-08-19 11:21:02 +0900

    원하는 단수 스캐너로 입력받아 출력

--- Ex2.java
+++ Ex2.java
@@ -3,20 +3,22 @@ package hw_ch4;
 //가희님 : 구구단 홀수단만 출력하게끔 밑에 코드 작성해서 수정해주세요.
 //정현님 : Scanner로 입력받아 원하는 단수를 출력하도록 코드 수정해주세요.
 
+import java.util.Scanner;
+
 public class Ex2 {
 
 	public static void main(String[] args) {
-		int dan;
-		int times;
 		
-		for (dan=2; dan<=9; dan++) {
-			for (times = 1; times<=9; times++) {
-				if (dan%2 == 0)
-					continue;
-				System.out.println(dan + "X"+ times + "="+dan*times);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("원하는 단수 >> ");
+		int dan = sc.nextInt();
+		
+
+			for(int times = 1; times <=9; times++) {
+				System.out.println(dan + "X" + times + "=" + dan*times);
 			}
 			System.out.println();
-		}
 	}
 
 }
